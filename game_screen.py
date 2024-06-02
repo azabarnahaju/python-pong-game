ad41a37950f281c4f09b2e539168c9eabb1cc89f
azabarnahaju <kanikalexandra@gmail.com>
2024-06-02 23:05:11 +0200

    setup game screen

---
+++ game_screen.py
@@ -0,0 +1,27 @@
+from turtle import Screen, Turtle
+
+
+class GameScreen:
+    def __init__(self):
+        self.screen = Screen()
+        self.__initial_setup()
+
+    def __initial_setup(self):
+        self.screen.tracer(0)
+        self.screen.setup(width=1000, height=600)
+        self.screen.bgcolor('black')
+        self.screen.title('Pong Game')
+        self.__create_half_line()
+        self.screen.update()
+
+    @staticmethod
+    def __create_half_line():
+        half_line = Turtle('square')
+        half_line.shapesize(stretch_wid=0.5, stretch_len=0.1)
+        half_line.color('white')
+        half_line.penup()
+        x, y = 0, 280
+        while y >= -275:
+            half_line.goto(x, y)
+            half_line.stamp()
+            y -= 20
---
+++ main.py
@@ -0,0 +1,15 @@
+# TODO: Create the screen
+# TODO: Create and move a paddle
+# TODO: Create another paddle
+# TODO: Create the ball and make it move
+# TODO: Detect collision with wall and bounce
+# TODO: Detect collision with pallet
+# TODO: Detect when paddle misses
+# TODO: Keep score
+
+from game_screen import GameScreen
+
+game_screen = GameScreen()
+
+game_screen.screen.exitonclick()
+
